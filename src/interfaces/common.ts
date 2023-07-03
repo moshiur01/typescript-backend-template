693be0572b6a8aeb0b986506444b3c7ab1738020
moshiur01 <mr.moshiur314@gmail.com>
2023-07-03 06:54:32 +0600

    [DONE] added some comments

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,7 +1,9 @@
-import { IGenericErrorMessage } from './error'
+import { IGenericErrorMessage } from './error';
 
 export type IGenericErrorResponse = {
-  statusCode: number
-  message: string
-  errorMessages: IGenericErrorMessage[]
-}
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+//Generic response message for response type
